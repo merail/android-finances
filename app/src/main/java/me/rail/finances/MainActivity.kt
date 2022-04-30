dbc773fdd439adc2e46b8496796ef7896a7a71e5
merail <r.mesherov@blanc.ru>
2022-04-30 16:00:49 +0300

    Preparing

--- app/src/main/java/me/rail/finances/MainActivity.kt
+++ app/src/main/java/me/rail/finances/MainActivity.kt
@@ -1,11 +1,13 @@
 package me.rail.finances
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
